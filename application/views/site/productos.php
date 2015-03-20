5072b8523c5b8d2d876d644223b101aa21aeaa83
Nicoxxx <njl27@hotmail.com>
2015-03-19 23:44:35 -0600

    detalles de productos

--- application/views/site/productos.php
+++ application/views/site/productos.php
@@ -1,8 +1,33 @@
+<div class="detailPro">
+	<div class="imagenPro">
+		<figure>
+			<img src="" alt="">
+		</figure>
+		<ul>
+			<?php foreach ($imagenes->result() as $img) { ?>
+			<li><img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt=""></li>
+			<?php } ?>
+			<li><img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt=""></li>
+			<li><img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt=""></li>
+			<li><img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt=""></li>
+			<li><img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt=""></li>
+			<li><img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt=""></li>
+			<li><img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt=""></li>
+			<li><img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt=""></li>
+			<li><img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt=""></li>
+			<li><img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt=""></li>
+			<li><img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt=""></li>
+			<li><img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt=""></li>
+			
+		</ul>
+	</div>
+	<div class="prodName">
+		<?php foreach ($product->result() as $p) { ?>
+			<h1><?=$p->nombreprod?></h1>
+			<p><?=$p->descripcion?></p>
+			<p>Precio $ <span><?=$p->precio?></span></p>
+		<?php } ?>
+	</div>
+</div>
 
-<section class="container">
-<?php foreach ($imagenes->result() as $img) { ?>
-<figure>
-	<img src="<?=base_url()?>uploads/<?=$img->ruta?>" alt="">
-</figure>
-<?php } ?>
-</section>
+<span>hola</span>
